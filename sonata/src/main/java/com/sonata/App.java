b12bfe08675aa9618ce0ebca6a1937d410a39eb5
KaveriHK <kavyahk27@gmail.com>
2017-07-11 11:10:06 +0530

    commit all

---
+++ sonata/src/main/java/com/sonata/App.java
@@ -0,0 +1,14 @@
+package com.sonata;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("This is Maven Project");
+    }
+}
